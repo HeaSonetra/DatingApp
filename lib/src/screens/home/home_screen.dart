e92324463c470e69be5493ae32e3d9bf32a262fa
leap <leapgio555@gmail.com>
2025-09-18 16:52:23 +0700

    mmpp

--- lib/src/screens/home/home_screen.dart
+++ lib/src/screens/home/home_screen.dart
@@ -7,7 +7,7 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('KhmerYou'),
+        title: const Text('ខ្មែរគូ'),
         actions: [
           IconButton(
             icon: const Icon(Icons.notifications),
